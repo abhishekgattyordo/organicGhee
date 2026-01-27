55190abee31b3ef6dc928894a1719c0062fb8ac0
abhishekgattyordo <abhishek.gatty@ordosolution.com>
2026-01-27 17:27:33 +0530

    Initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -6,18 +6,12 @@
 
 
 
-
-import React from "react";
+import React from "react";  
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom"; // ⬅️ Add this
 import App from "./App.tsx";
 import "./index.css";
 
 const container = document.getElementById("root");
 if (!container) throw new Error("Root element not found");
 
-createRoot(container).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+createRoot(container).render(<App />);
